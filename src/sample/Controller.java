e788dca37269df9ac4dc0ea0c7bdd7b1fdef4234
Aleksandr <menon2@live.ru>
2021-05-03 18:14:35 +0300

    version_0.1.0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.lang.reflect.Field;
+
 public class Controller {
+
+    @FXML
+    private Button btn;
+    public TextField messageField;
+    public TextArea mainChat;
+
+    public void btnClick(ActionEvent actionEvent) {
+
+        mainChat.setText(mainChat.getText() + messageField.getText() + "\n");
+        messageField.clear();
+    }
 }
